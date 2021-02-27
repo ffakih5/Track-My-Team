14a94b41f22d3b2e41c3bae2fcc0053c2ffa4768
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-02-27 22:51:53 +1100

    added inquirer questions and answer switch-case-break from class example

--- app.js
+++ app.js
@@ -10,21 +10,69 @@ const connection = mysql.createConnection({
     database: 'team_db',
 });
 
-//remember to close connection! #memory issues! 
-const afterConnection = () =>{
-    connection.query('SELECT * FROM employee',(err, res) => {
-        if (err) throw err;
-        console.log(res);
-        connection.end();
-
-
-    })
-}
-
 connection.connect((err) =>{
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}`);
     afterConnection();
     
 });
+
+const runSearch = () => {
+    inquirer
+      .prompt({
+        name: 'action',
+        type: 'rawlist',
+        message: 'What would you like to do?',
+        choices: [
+          'Find songs by artist',
+          'Find all artists who appear more than once',
+          'Find data within a specific range',
+          'Search for a specific song',
+          'Find artists with a top song and top album in the same year',
+        ],
+      })
+      .then((answer) => {
+        switch (answer.action) {
+          case 'Find songs by artist':
+            artistSearch();
+            break;
+  
+          case 'Find all artists who appear more than once':
+            multiSearch();
+            break;
+  
+          case 'Find data within a specific range':
+            rangeSearch();
+            break;
+  
+          case 'Search for a specific song':
+            songSearch();
+            break;
+  
+          case 'Find artists with a top song and top album in the same year':
+            songAndAlbumSearch();
+            break;
+  
+          default:
+            console.log(`Invalid action: ${answer.action}`);
+            break;
+        }
+      });
+  };
+  
+
+//remember to close connection! #memory issues! 
+const afterConnection = () => {
+    connection.query('SELECT * FROM employee',(err, res) => {
+        if (err) throw err;
+        console.log(res);
+
+
+
+
+        connection.end();
+
+
+    })
+}
 
